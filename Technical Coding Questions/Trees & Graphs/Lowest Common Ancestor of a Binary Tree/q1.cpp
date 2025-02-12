6ce7dc5388376ebe6d9a16f8b8f2ce2ecaea1c7b
abhinavshreshth <123002118+abhinavshreshth@users.noreply.github.com>
2025-02-12 09:05:39 +0530

    Add implementation for Lowest Common Ancestor of a Binary Tree

---
+++ Technical Coding Questions/Trees & Graphs/Lowest Common Ancestor of a Binary Tree/q1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+         val = x ; 
+         left = NULL;
+         right = NULL;
+        }
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // Base case: if root is NULL or root is one of p or q
+        if (root == NULL || root == p || root == q)
+            return root;
+
+        // Recursively search in left and right subtrees
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        // If both left and right return non-null, root is LCA
+        if (left && right) 
+            return root;
+
+        // Otherwise, return non-null child (either left or right)
+        return left ? left : right;
+    }
+};
+
+// Helper function to create a sample tree
+TreeNode* createSampleTree() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(1);
+    root->left->left = new TreeNode(6);
+    root->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(0);
+    root->right->right = new TreeNode(8);
+    root->left->right->left = new TreeNode(7);
+    root->left->right->right = new TreeNode(4);
+    return root;
+}
+
+// Driver code to test LCA function
+int main() {
+    TreeNode* root = createSampleTree();
+    Solution sol;
+
+    TreeNode* p = root->left;  // Node with value 5
+    TreeNode* q = root->right; // Node with value 1
+
+    TreeNode* lca = sol.lowestCommonAncestor(root, p, q);
+    cout << "LCA of " << p->val << " and " << q->val << " is: " << lca->val << endl;
+
+    return 0;
+}
